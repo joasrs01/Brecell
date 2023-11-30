bc85be800d1d78069b103289fdd60c1538502d37
joasrs <joas_rsouza@outlook.com>
2023-11-29 21:48:08 -0300

    adicionado switch pra controlar as requisições

--- api.php
+++ api.php
@@ -6,28 +6,28 @@ require_once "lib/biblioteca.php";
 
     switch ($acao) {
         case 'GET':
-            
+
             // $tarefas = lerListaTarefas();
             // header('Content-type: application/json');
             // echo json_encode($tarefas);
         break;
         case 'POST':
-            $tarefa = json_decode(file_get_contents('php://input'),true);
-            $tarefa = adicionarTarefa($tarefa['descricao']);
-            http_response_code(201);
-            header('Content-type: application/json');
-            echo json_encode($tarefa);
+            // $tarefa = json_decode(file_get_contents('php://input'),true);
+            // $tarefa = adicionarTarefa($tarefa['descricao']);
+            // http_response_code(201);
+            // header('Content-type: application/json');
+            // echo json_encode($tarefa);
             break;
         case 'PUT':
-            $tarefa = json_decode(file_get_contents('php://input'),true);
-            $tarefa = editarTarefa($tarefa['id'],$tarefa['descricao'],$tarefa['finalizado']);
-            http_response_code(201);
-            header('Content-type: application/json');
-            echo json_encode($tarefa);
+            // $tarefa = json_decode(file_get_contents('php://input'),true);
+            // $tarefa = editarTarefa($tarefa['id'],$tarefa['descricao'],$tarefa['finalizado']);
+            // http_response_code(201);
+            // header('Content-type: application/json');
+            // echo json_encode($tarefa);
             break;
         case 'DELETE': 
-            $id = $_GET['id'];
-            excluirTarefa($id);
+            // $id = $_GET['id'];
+            // excluirTarefa($id);
         break;
     }
 
